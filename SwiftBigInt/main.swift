90b7366ce93b2ffe28b317b3336ff49a649ad94a
Erik <nuclear.ace@gmail.com>
2015-07-01 10:19:56 -0400

    first commit

---
+++ SwiftBigInt/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftBigInt
+//
+//  Created by Erik Little on 7/1/15.
+//  Copyright © 2015 Erik Little. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
